768c02aa1b32686f54ad6df46d47ee1769d16668
jadavnishith <mrnick076@gmail.com>
2024-07-19 14:51:48 +0530

    R & W Clone

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('h2').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 1000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
